5d91e8dbef4c1d4f414cb737e12922b54b8c6a03
Mariia <mariia_merkel@mail.ru>
2024-01-09 12:33:30 +0300

    выполнила домашку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("task 1:");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName + ".";
+        System.out.println(fullName);
+
+        System.out.println("task 2:");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("task 3:");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace('ё', 'е'));
     }
 }
