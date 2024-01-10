0efbe4e5af2385b74963986d7c0b861903b73a5c
Mariia <mariia_merkel@mail.ru>
2024-01-10 15:16:08 +0300

    выполнила дополнительные задания

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,105 @@
-import java.util.HashSet;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        splitFullName("Ivanov Ivan Ivanovich");
-        changeFirstChar("ivanov ivan ivanovich");
-        mixTwoArray("135", "246");
-        mixTwoArrayWithSB("135", "246");
-        deleteDuplicate("aabccddefgghiijjkk");
+        System.out.println(doubleChars("The"));
+        System.out.println(setMiddleString("<<>>", "hi"));
+        System.out.println(concatenateFirstAndLastCharacters("", "gfhf"));
+        System.out.println(swapLastTwoCharacters("ba"));
+        System.out.println(concatenatePartiallyCutWords("Hello", "JAVA"));
+        System.out.println(countOfSubstrings("parampampampam", "pam"));
+        System.out.println(repeatLastFewCharacters("parampamlam", 5));
+        System.out.println(countIdenticalConsecutiveCharacters("ddfffee"));
+        System.out.println(repeatFewTimesWithSeparator("hello", "-1-", 5));
     }
 
-    public static void splitFullName(String fullName) {
-        String firstName = fullName.substring(0, fullName.indexOf(" ")).trim();
-        String middleName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" ")).trim();
-        String secondName = fullName.substring(fullName.lastIndexOf(" ")).trim();
-        System.out.println("Имя сотрудника — " + firstName + "\nФамилия сотрудника — " + middleName + "\nОтчество сотрудника — " + secondName);
-    }
-    
-    public static void changeFirstChar(String fullName) {
-        String firstName = fullName.substring(0, fullName.indexOf(" ")).trim();
-        String middleName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" ")).trim();
-        String secondName = fullName.substring(fullName.lastIndexOf(" ")).trim();
-        char[] charsFirstName = firstName.toCharArray();
-        char[] charsMiddleName = middleName.toCharArray();
-        char[] charsSecondName = secondName.toCharArray();
-        charsFirstName[0] = Character.toUpperCase(charsFirstName[0]);
-        charsMiddleName[0] = Character.toUpperCase(charsMiddleName[0]);
-        charsSecondName[0] = Character.toUpperCase(charsSecondName[0]);
-        String [] strings = new String[] {String.valueOf(charsFirstName), String.valueOf(charsMiddleName), String.valueOf(charsSecondName)};
-        String  changedFullName = String.join(" ", strings);
-        System.out.println(changedFullName);
-    }
-    public static void mixTwoArray(String str1, String str2) {
-        if (str1.length() == str2.length()) {
-            char[] chars1 = str1.toCharArray();
-            char[] chars2 = str2.toCharArray();
-            char[] fullChars = new char[chars1.length + chars2.length];
-            for(int i = 0; i < fullChars.length;) {
-                for(int j = 0; j < chars1.length; j++) {
-                    fullChars[i++] = chars1[j];
-                    fullChars[i++] = chars2[j];
-                }
-            }
-            String fullString = String.valueOf(fullChars);
-            System.out.println(fullString);
-        }
-    }
-    public static void mixTwoArrayWithSB(String str1, String str2) {
-        if (str1.length() == str2.length()) {
-            char[] chars1 = str1.toCharArray();
-            char[] chars2 = str2.toCharArray();
-            StringBuilder finalStr = new StringBuilder();
-            for (int i = 0; i < str1.length(); i++){
-                finalStr.append(chars1[i]);
-                finalStr.append(chars2[i]);
-            }
-            System.out.println(finalStr);
-        }
-    }
-
-    public static void deleteDuplicate(String str) {
+    public static String doubleChars(String str) {
         char[] chars = str.toCharArray();
-        HashSet hashSet = new HashSet<>();
-        for (int i = 0; i<chars.length; i++) {
-            hashSet.add(chars[i]);
+        char[] doubleChars = new char[chars.length * 2];
+        int j = 0;
+        for (char c : chars) {
+            doubleChars[j++] = c;
+            doubleChars[j++] = c;
         }
-        StringBuilder newString = new StringBuilder();
-        hashSet.forEach(hs -> newString.append(hs));
-        System.out.println(newString);
+        return String.valueOf(doubleChars);
     }
 
+    public static String setMiddleString(String str, String middleStr) {
+        int middle = str.length() / 2;
+        return str.substring(0, middle) + middleStr + str.substring(middle);
+    }
+
+    public static String concatenateFirstAndLastCharacters(String first, String last) {
+        String result = "";
+        if (first.length() != 0) {
+            result = Character.toString(first.charAt(0));
+        } else {
+            result = Character.toString('@');
+        }
+
+        if (last.length() != 0) {
+            result += last.charAt(last.length() - 1);
+        } else {
+            result += Character.toString('@');
+        }
+        return result;
+    }
+
+    public static String swapLastTwoCharacters(String string) {
+        int indexOfPenultimateCharacter = string.length() - 2;
+        char lastCharacter = string.charAt(string.length() - 1);
+        char penultimateCharacter = string.charAt(indexOfPenultimateCharacter);
+        String result = string.substring(0, indexOfPenultimateCharacter) + lastCharacter + penultimateCharacter;
+        return result;
+    }
+
+    public static String concatenatePartiallyCutWords(String oneString, String twoString) {
+        int length = Math.min(oneString.length(), twoString.length());
+        return oneString.substring(0, length) + twoString.substring(0, length);
+    }
+
+    public static int countOfSubstrings(String string, String subString) {
+        int c = 0;
+        while (string.contains(subString)) {
+            string = string.substring(string.indexOf(subString) + subString.length());
+            c++;
+        }
+        return c;
+    }
+
+    public static String repeatLastFewCharacters(String string, int few) {
+        String result = "";
+        for (int i = few; i > 0; i--) {
+            result += string.substring(string.length() - few);
+        }
+        return result;
+    }
+
+    public static int countIdenticalConsecutiveCharacters(String string) {
+        int count = Math.min(1, string.length());
+        int maxCount = Math.min(1, string.length());
+        for (int i = 0; i < string.length() - 1; ) {
+            if (string.charAt(i) == string.charAt(++i)) {
+                count++;
+                maxCount = Math.max(count, maxCount);
+            } else {
+                count = 1;
+            }
+        }
+        return maxCount;
+    }
+
+    public static String repeatFewTimesWithSeparator(String string, String separator, int few) {
+        if (few > 0) {
+            int i = few - 1;
+            String result = string;
+            while (i > 0) {
+                result += separator + string;
+                i--;
+            }
+            return result;
+        } else {
+            return "";
+        }
+    }
 }
